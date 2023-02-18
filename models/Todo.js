9ff0105e4a98862c3b282957683ba26f28e002fe
JoshuaK0 <114297073+JoshuaK0@users.noreply.github.com>
2023-02-18 21:19:04 +0000

    initial commit

---
+++ models/Todo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Scheme = mongoose.Schema;
+
+const TodoSchema = new Scheme
+({
+    text:
+    {
+        type: String,
+        required: true
+    },
+    complete:
+    {
+        type: Boolean,
+        default: false
+    },
+    timestamp:
+    {
+        type: String,
+        default: Date.now()
+    }
+})
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
