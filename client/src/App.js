46fd2ab9e48d5269256e946fd3939b9aec4c209e
JoshuaK0 <114297073+JoshuaK0@users.noreply.github.com>
2023-02-19 05:19:03 +0000

    Update server URL

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const API_BASE = "https://mern-todo-list-web-app-backend.onrender.com";
+const API_BASE = "https://mern-todo-webapp-backend.onrender.com";
 
 function App()
 {
