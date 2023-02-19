ba287f34e1bff7f5d80b5fa0d366e20b8e350796
JoshuaK0 <114297073+JoshuaK0@users.noreply.github.com>
2023-02-19 05:15:39 +0000

    Update server settings

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todo-list-web-app",
+mongoose.connect("mongodb://mern-todo-list-web-app-backend.onrender.com",
     {
     useNewUrlParser: true,
     useUnifiedTopology: true
